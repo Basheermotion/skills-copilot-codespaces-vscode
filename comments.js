05e2267d3fecd22d6c32bbfe6a2d1b65cbd5d0b5
Basheer <61949728+Basheermotion@users.noreply.github.com>
2023-07-19 20:48:42 +0000

    Copilot tirhd commit

--- comments.js
+++ comments.js
@@ -1,14 +1,42 @@
 // Create server web comments
-// ========================================================
+// Date: 12/03/2021
+// Programmer: Bruno Oliveira - Version 1.0
 
-// Import Node.js core dependencies
-const fs = require('fs');
-const path = require('path');
-
-// Import third-party dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
+const router = express.Router();
+const Comment = require('../models/comment');
+const config = require('../config/database');
 
-// Constants
-const PORT = 3000;
-const HOST = '
+// Add comment
+router.post('/add', (req, res, next) => {
+    let newComment = new Comment({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    });
+    Comment.addComment(newComment, (err, comment) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to add comment' });
+        } else {
+            res.json({ success: true, msg: 'Comment added' });
+        }
+    });
+});
+
+// Get all comments
+router.get('/get', (req, res, next) => {
+    Comment.getComments((err, comments) => {
+        if (err) throw err;
+        res.json(comments);
+    });
+});
+
+// Delete comment
+router.delete('/delete/:id', (req, res, next) => {
+    Comment.deleteComment(req.params.id, (err, comment) => {
+        if (err) throw err;
+        res.json({ success: true, msg: 'Comment deleted' });
+    });
+});
+
+module.exports = router;
