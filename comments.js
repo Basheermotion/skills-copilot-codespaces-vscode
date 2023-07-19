5b7fbc6fb6dbf01acece498b459f31bf2446dade
Basheer <61949728+Basheermotion@users.noreply.github.com>
2023-07-19 19:19:49 +0000

    Copilot third comit

--- comments.js
+++ comments.js
@@ -1,79 +1,36 @@
-// web server: localhost:3000
-// Description: This file handles all the routes for comments
-//              It also handles the logic for creating comments and 
-//              updating comments
-//***********************************************************
-
-//***********************************************************
-// External Imports
-//***********************************************************
+// Creat web app with express
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../models');
-const { User } = require('../models');
-const { Post } = require('../models');
-const { authMiddleware } = require('../middleware/auth');
+// Get model
+const Comment = require('../models/comment');
+const Post = require('../models/post');
 
-//***********************************************************
 // Get all comments
-//***********************************************************
-router.get('/', authMiddleware, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const comments = await Comment.findAll();
+        const comments = await Comment.find();
         res.json(comments);
     } catch (err) {
-        res.status(500).json({ error: err });
+        res.json({ message: err });
     }
 });
 
-//***********************************************************
-// Get all comments for a post
-//***********************************************************
-router.get('/post/:post_id', authMiddleware, async (req, res) => {
+// Get all comments of a post
+router.get('/:postId', async (req, res) => {
     try {
-        const comments = await Comment.findAll({
-            where: { post_id: req.params.post_id },
-            include: [
-                {
-                    model: User,
-                    attributes: ['username', 'id']
-                }
-            ]
-        });
+        const comments = await Comment.find({ postId: req.params.postId });
         res.json(comments);
     } catch (err) {
-        res.status(500).json({ error: err });
+        res.json({ message: err });
     }
 });
 
-//***********************************************************
-// Get all comments for a user
-//***********************************************************
-router.get('/user/:user_id', authMiddleware, async (req, res) => {
-    try {
-        const comments = await Comment.findAll({
-            where: { user_id: req.params.user_id },
-            include: [
-                {
-                    model: Post,
-                    attributes: ['id', 'title', 'content']
-                }
-            ]
-        });
-        res.json(comments);
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-});
-
-//***********************************************************
 // Get a comment by id
-//***********************************************************
-router.get('/:id', authMiddleware, async (req, res) => {
+router.get('/comment/:commentId', async (req, res) => {
     try {
-        const comment = await Comment.findByPk(req.params.id);
+        const comment = await Comment.findById(req.params.commentId);
         res.json(comment);
     } catch (err) {
-        res.status(500).json({ error: err });
+        res.json({ message: err });
     }
 });
